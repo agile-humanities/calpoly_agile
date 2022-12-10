8a62f310291d2d158f9cc80ca3c41a88bdd3d199
Alan Stanley <alanjarlathstanley@gmail.com>
2022-12-10 17:27:44 -0400

    Tweaked the message

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -36,23 +36,23 @@ class RedirectController extends ControllerBase {
   }
 
   /**
-   * Redirect
+   * reklRedirect
    *
    * @return RedirectResponse
    *   Return new destination.
    */
-  public function reklRedirect() {
+    public function reklRedirect() {
     $uri = $this->requestStack->getMasterRequest()->getRequestUri();
     $path_parts = \explode('/', $uri);
     $destination = '/';
+    $message = $this->t("The page you are looking for - $uri - does not exist on this site");
 
     if (substr($path_parts[1], 0, 4) == 'rekl') {
       $rekl = \str_replace('_', ':', $path_parts[1]);
       $destination = "/solr-search/content?search_api_fulltext=$rekl&sort_by=field_edtf_date_created&sort_order=ASC&items_per_page=10";
-      $this->messenger->addStatus("You have arrived here using a URL from our old site");
-      $this->messenger->addStatus("We hope this will help you find what you are looking for.");
+      $message = $this->t("You have arrived here using a URL from our old site. \nWe hope this will help you find what you are looking for.");
     }
+    $this->messenger->addStatus($message);
     return new RedirectResponse($destination);
   }
-
 }
