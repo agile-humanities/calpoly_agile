eb96ccce930917d8cf7cdd53ef25476e612edd0e
ajstanley <alanjarlathstanley@gmail.com>
2023-05-04 15:32:56 -0300

    Added middleware in keeping with new Drupal standards

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -36,25 +36,22 @@ class RedirectController extends ControllerBase {
   }
 
   /**
-   * reklRedirect
-   *
-   * @return RedirectResponse
-   *   Return new destination.
+   * Redirects calls from old site.
    */
-       public function reklRedirect() {                                
+  public function reklRedirect() {
     $uri = $this->requestStack->getMasterRequest()->getRequestUri();
     $path_parts = \explode('/', $uri);
-    $destination = '/';                                                                      
+    $destination = '/';
     $message = $this->t("The page you are looking for - $uri - does not exist on this site");
-                                                 
-    if (substr($path_parts[1], 0, 4) == 'rekl') {    
-      $rekl = \str_replace('_', ':', $path_parts[1]);                                                                                            
-      $destination = "/solr-search/content?search_api_fulltext=$rekl&sort_by=field_edtf_date_created&sort_order=ASC&items_per_page=10";          
+
+    if (substr($path_parts[1], 0, 4) == 'rekl') {
+      $rekl = \str_replace('_', ':', $path_parts[1]);
+      $destination = "/solr-search/content?search_api_fulltext=$rekl&sort_by=field_edtf_date_created&sort_order=ASC&items_per_page=10";
       $message = $this->t("You have arrived here using a URL from our old site. \nWe hope this will help you find what you are looking for.");
-    }                                                                                                                                         
-    $this->messenger->addMessage($message);                                                                                                   
-    $response = new RedirectResponse($destination);         
-    $response->send();                             
-    return $response;                              
+    }
+    $this->messenger->addMessage($message);
+    $response = new RedirectResponse($destination, 302);
+    \Drupal::service('http_middleware.calpoly_agile_redirect')->setRedirectResponse($response);
+    return;
   }
 }
